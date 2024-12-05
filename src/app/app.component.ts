b93644af47bf981a0bdbe6a4a824969e67e39b17
rv-2002 <raguvaranraguvaran020@gmail.com>
2024-12-05 12:18:34 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ragu';
+  name='ragu';
+  age=23;
+  course='ECE';
+  year=3;
+}
